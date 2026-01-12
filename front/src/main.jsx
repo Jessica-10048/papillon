db747a7d935eee2abd45f62b1848a3e5f2e08384
Jessica10048 <jessicajulie60@msn.com>
2026-01-12 12:09:59 +0100

    ajout style.css dans main.jsx

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import '../src/css/style.css'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router";
 import { AuthProvider } from './utilis/context/AuthContext.jsx';
